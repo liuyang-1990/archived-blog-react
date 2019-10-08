4baabbb70bb86aefbfe55ec54175904b9e5c4146
liuyang-1990 <xy_liuy0305@163.com>
2019-10-08 22:20:54 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import routers from './config/router.config';
+import Layouts from './layouts/layout';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<any, any>{
+
+  render() {
+    return (
+      <Router>
+        <ConfigProvider locale={zh_CN}>
+          <Switch>
+            <Layouts>
+              {routers.map((r, key) => (
+                <Route
+                  // component={r.component}
+                  exact={!!r.exact}
+                  key={key}
+                  path={r.path}
+                />
+              ))}
+            </Layouts>
+
+          </Switch>
+        </ConfigProvider>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/config/router.config.ts
@@ -0,0 +1,49 @@
+const config = [
+    {
+        name: '/',
+        path: '/',
+        exact: true,
+    },
+    {
+        name: 'articles',
+        path: '/articles',
+        exact: true
+    },
+    {
+        name: 'hot',
+        path: '/hot',
+        exact: true
+    },
+    {
+        name: 'archive',
+        path: '/archive',
+        exact: true,
+    },
+    {
+        name: 'timeLine',
+        path: '/timeLine',
+        exact: true,
+    },
+    {
+        name: 'message',
+        path: '/message',
+        exact: true
+    },
+    {
+        name: 'about',
+        path: '/about',
+        exact: true
+    },
+    {
+        name: 'articleDetail',
+        path: '/articleDetail',
+        exact: true,
+    },
+    {
+        name: 'project',
+        path: '/project',
+        exact: true,
+    },
+];
+
+export default config;
